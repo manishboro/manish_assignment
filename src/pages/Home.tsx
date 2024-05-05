22ddc6ec23f6940507d01c98fcc088a7930c0ebb
manishboro <manish.boro@emitrr.com>
2024-05-05 11:47:59 +0530

    fetch jobs data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,45 @@
-import { Box } from '@mui/material';
+import { Box, CircularProgress, Container } from '@mui/material';
+
+import { useFetch } from 'hooks/useFetch';
+import { JDList } from 'libs/types/jobDescription';
 
 const Home = () => {
-  return <Box>hello world</Box>;
+  const { data, isError, isFetching } = useFetch<JDList>({
+    url: `https://api.weekday.technology/adhoc/getSampleJdJSON`,
+    method: 'POST',
+    body: { limit: 10, offset: 0 },
+  });
+
+  console.log('data', data);
+
+  return (
+    <Container maxWidth="lg" sx={{ paddingTop: '2rem', paddingBottom: '2rem' }}>
+      {isError ? (
+        <Box>Failed to fetch.</Box>
+      ) : isFetching && !data?.jdList.length ? (
+        <Box sx={{ margin: 'auto', width: 'max-content' }}>
+          <CircularProgress />
+        </Box>
+      ) : (
+        <Box
+          sx={{
+            gap: '2rem',
+            display: 'flex',
+            justifyContent: 'center',
+            flexWrap: 'wrap',
+          }}
+        >
+          {Array.isArray(data?.jdList) ? (
+            data.jdList.length ? (
+              data.jdList.map((jd) => <Box key={jd.jdUid}>{jd.companyName}</Box>)
+            ) : (
+              <Box>No jobs found</Box>
+            )
+          ) : null}
+        </Box>
+      )}
+    </Container>
+  );
 };
 
 export default Home;
